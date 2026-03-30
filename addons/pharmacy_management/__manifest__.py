3c413c49465ef5f84fcb4ad4210dda9ba6e3ade9
Abdelrahman Farghali <me@abdelrahmanfarghali.link>
2026-03-30 19:22:57 +0200

    project files

---
+++ addons/pharmacy_management/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Pharmacy Management System',
+    'version': '1.0',
+    'summary': 'Manage pharmacy operations',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
